7610d6b222b6513abfbcfa0d7d6d6b79e3b92550
Sophie Worsnop <sophie.g.worsnop@gmail.com>
2020-10-29 13:25:56 +0000

    Removes login page route

--- src/index.js
+++ src/index.js
@@ -9,7 +9,7 @@ import "assets/scss/material-kit-react.scss?v=1.9.0";
 import Components from "views/Components/Components.js";
 import LandingPage from "views/LandingPage/LandingPage.js";
 import ProfilePage from "views/ProfilePage/ProfilePage.js";
-import LoginPage from "views/LoginPage/LoginPage.js";
+// import LoginPage from "views/LoginPage/LoginPage.js";
 
 var hist = createBrowserHistory();
 
@@ -18,7 +18,7 @@ ReactDOM.render(
     <Switch>
       <Route path="/" component={LandingPage} />
       <Route path="/profile-page" component={ProfilePage} />
-      <Route path="/login-page" component={LoginPage} />
+      {/* <Route path="/login-page" component={LoginPage} /> */}
       <Route path="/components" component={Components} />
     </Switch>
   </Router>,
