ff776c4a8f9c17c806732d9d3c085eb31c628c06
Sophie Worsnop <sophie.g.worsnop@gmail.com>
2020-11-03 14:06:09 +0000

    Creates architecture view

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import { Router, Route, Switch } from "react-router-dom";
 import "assets/scss/material-kit-react.scss?v=1.9.0";
 
 // pages for this product
-import Components from "views/Components/Components.js";
+// import Components from "views/Components/Components.js";
 import LandingPage from "views/LandingPage/LandingPage.js";
 import ProfilePage from "views/ProfilePage/ProfilePage.js";
 // import LoginPage from "views/LoginPage/LoginPage.js";
@@ -16,10 +16,11 @@ var hist = createBrowserHistory();
 ReactDOM.render(
   <Router history={hist}>
     <Switch>
-      <Route path="/" component={LandingPage} />
       <Route path="/profile-page" component={ProfilePage} />
-      {/* <Route path="/login-page" component={LoginPage} /> */}
-      <Route path="/components" component={Components} />
+       {/* <Route path="/login-page" component={LoginPage} /> */}
+      {/* <Route path="/components" component={Components} /> */}
+      <Route path="/" exact component={LandingPage} />
+
     </Switch>
   </Router>,
   document.getElementById("root")
