261fb57a471420a15ff985e8ef217c1519a1244c
Sophie Worsnop <sophie.g.worsnop@gmail.com>
2020-12-08 17:43:44 +0000

    Renames files associated with academic work

--- src/App.js
+++ src/App.js
@@ -2,18 +2,18 @@ import React from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom"
 import { createBrowserHistory } from "history";
 
-import LandingPage from "views/LandingPage/LandingPage.js";
-import ArchitecturePortfolio from "views/ArchitecturePortfolio/ArchitecturePortfolio.js"
+import LandingPage from "views/LandingPage/LandingPage";
+import AcademicWork from "views/AcademicWork/AcademicWork"
 import ProfessionalWork from "views/ProfessionalWork/ProfessionalWork";
 
-function App(props) {
+function App() {
 
   var hist = createBrowserHistory()
 
   return (
     <div>
       <Router history={hist}>
-        <Route path="/academic-portfolio" exact component={ArchitecturePortfolio} />
+        <Route path="/academic-portfolio" exact component={AcademicWork} />
         <Route path="/professional-portfolio" exact component={ProfessionalWork} />
         <Route path="/" exact component={LandingPage} />
       </Router>
